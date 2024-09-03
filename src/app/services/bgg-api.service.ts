1636cbc08eda8dc68c6a2935e6cc67e047df6fa5
patrikwolle <patrik.wolle@googlemail.com>
2024-09-03 20:56:35 +0200

    Update API headers and XML response handling
    Added HttpHeaders to requests in BggApiService to allow CORS. Modified XML response handling to alert the user when the request is not ready. Also, updated an icon in the list page for UI consistency.

--- src/app/services/bgg-api.service.ts
+++ src/app/services/bgg-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { switchMap } from 'rxjs/operators';
 import { BggResponse, IBggResponse } from './models/bgg-response.model';
 import * as xml2js from 'xml2js';
@@ -9,20 +9,32 @@ import * as xml2js from 'xml2js';
 export class BggApiService {
   constructor(private http: HttpClient) {}
 
+  headers = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
+
   getUserCollection(username: string) {
-    //username = username || 'Leviasa';
     return this.http
       .get(
-        `https://boardgamegeek.com/xmlapi2/collection?username=${username}&stats=1&excludesubtype=boardgameexpansion`,
+        `api/xmlapi2/collection?username=${username}&showprivate=1&stats=1`,
+
         {
+          headers: this.headers,
           responseType: 'text',
-          withCredentials: 'true',
+          withCredentials: true,
         },
       )
       .pipe(switchMap(async (xml) => await this.parseXmlToJson(xml)));
   }
 
   async parseXmlToJson(xml: string) {
+    if (
+      xml ===
+      `<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<message>
+\tYour request for this collection has been accepted and will be processed.  Please try again later for access.
+</message>`
+    ) {
+      alert('Request not ready');
+    }
     const parsedData = await xml2js.parseStringPromise(xml, {
       explicitArray: false,
     });
